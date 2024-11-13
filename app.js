d4ca280ad9a8a44fc8efa829fd36b28ce897b766
ryandbarnett <conner2468@gmail.com>
2024-11-13 14:17:51 -0700

    Add user registration route with bcrypt password hashing

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Import and use auth routes
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
+
 // Basic route to test server
 app.get('/', (req, res) => {
   res.send('Express and dotenv are working!');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
